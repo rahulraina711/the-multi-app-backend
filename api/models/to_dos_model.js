319f913ef2386dff8e2e3d32692346fa670058a9
rahul Raina <rahulraina711@gmail.com>
2021-05-04 04:59:14 +0530

    rectifying

--- api/models/to_dos_model.js
+++ api/models/to_dos_model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const commentSchema= new mongoose.Schema({
+const todoSchema= new mongoose.Schema({
     comment: {type: String, required: true},
     postId: {type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
     userId:{type: String, required:true}
 },
 {timestamps:true});
 
-module.exports = mongoose.model("Comment", commentSchema);
+module.exports = mongoose.model("Todo", todoSchema);
--- api/routes/flag_route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const FlagController = require('../controllers/flag_controller');
-
-router.post("/:id", FlagController.flag_post);
-
-module.exports = router;
